807c2eaa616ea27a354303d14b0d7bc279760fa8
Madhav <68652055+kmsaryan@users.noreply.github.com>
2025-02-27 09:44:38 +0100

    added procfile

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 import random
 
-app = Flask(__name__)
-CORS(app)  # Allow all origins
+app = Flask(__name__, static_folder='static', template_folder='templates')
+CORS(app)
 
 players = []
 matches = {}
 
 @app.route('/')
 def home():
-    return "Flask Backend is Running!"
+    return render_template('index.html')  # Serve the frontend
 
 @app.route('/register', methods=['POST'])
 def register():
